5bbf686a79a2f83a8cc76a2244b0ae4e375a64ad
Danilo Abranches <daniloeabranches@gmail.com>
2020-03-19 20:51:56 -0300

    Exclusão do módulo common

--- src/common/src/main/java/com/exemplo/astroimagemdodia/common/observer/Observable.kt
+++
@@ -1,8 +0,0 @@
-package com.exemplo.astroimagemdodia.common.observer
-
-class Observable : java.util.Observable() {
-    fun setData(data: Any?){
-        setChanged()
-        notifyObservers(data)
-    }
-}
